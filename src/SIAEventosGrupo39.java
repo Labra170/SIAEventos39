977b6119b2a1b5fae0df08496760c7eabcf9d868
Labra170 <78867359+Labra170@users.noreply.github.com>
2024-09-06 20:48:32 -0400

    Creación menú
    El menú ya es operativo

--- src/SIAEventosGrupo39.java
+++ src/SIAEventosGrupo39.java
@@ -21,6 +21,73 @@ public class SIAEventosGrupo39 {
             System.out.println();
             opcion = Integer.parseInt( ingresado );
             if (opcion == 0) {System.out.println("Hasta la proxima");break;}
+            switch (opcion)
+            {
+                case 1 -> // Agregar lugar
+                {
+                    Lugar lugar = new Lugar();
+                    System.out.print("Ingresa el nombre: ");
+                    lugar.setNombre(lector.readLine());
+                    System.out.print("Ingresa la direccion: ");
+                    lugar.setDireccion(lector.readLine());
+                    System.out.print("Ingresa la capacidad de asistentes: ");
+                    lugar.setCapacidad(lector.readLine());
+                    lugares.add(lugar);
+                }
+                case 2 -> // Mostrar lugares
+                {
+                    for (int i = 0; i < lugares.size(); i++)
+                    {
+                        System.out.println("Nombre: " + lugares.get(i).getNombre() + " Direccion: " + lugares.get(i).getDireccion());
+                    }
+                }
+                case 3 -> // Agregar evento
+                {
+                    int i = 0;
+                    while (true)
+                    {
+                        if (i == 0)
+                        {
+                        System.out.print("En donde quieres agregar el evento? ");
+                        ingresado = lector.readLine();
+                        }
+                        if (lugares.get(i).getNombre().equals(ingresado))
+                        {
+                            lugares.get(i).anadirEvento();
+                            break;
+                        }
+                        if (i == lugares.size()-1)
+                        {
+                            System.out.println("El lugar no existe! Intenta nuevamente");
+                            i = 0;
+                        }
+                        else i++;
+                    }
+                }
+                case 4 ->
+                {
+                    int i = 0;
+                    while (true)
+                    {
+                        if (i == 0)
+                        {
+                            System.out.print("De que lugar quieres ver la lista? ");
+                            ingresado = lector.readLine();
+                        }
+                        if (lugares.get(i).getNombre().equals(ingresado))
+                        {
+                            lugares.get(i).mostrarEventos();
+                            break;
+                        }
+                        if (i == lugares.size()-1)
+                        {
+                            System.out.println("El lugar no existe! Intenta nuevamente");
+                            i = 0;
+                        }
+                        else i++;
+                    }
+                }
+            }
         }
     }
 }
