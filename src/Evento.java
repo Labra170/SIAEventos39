a779140f10ad89768dd3281e42e59eb8982e2281
Labra170 <78867359+Labra170@users.noreply.github.com>
2024-09-06 20:44:49 -0400

    Creación métodos

--- src/Evento.java
+++ src/Evento.java
@@ -10,4 +10,37 @@ public class Evento
         tipo = "Pred";
     }
     
+    // Setters y Getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAsistentes() {
+        return asistentes;
+    }
+
+    public void setAsistentes(int asistentes) {
+        this.asistentes = asistentes;
+    }
+    public void setAsistentes(String asistentes) // Sobrecarga setter, en caso de tener caracteres invalidos para el atributo
+    {
+        asistentes = asistentes.replaceAll("[^0-9]", " ");
+        asistentes = asistentes.replaceAll(" +", "");
+        System.out.println(asistentes);
+        this.asistentes = Integer.parseInt(asistentes);
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
 }
--- src/Lugar.java
+++ src/Lugar.java
@@ -15,4 +15,64 @@ public class Lugar {
         eventos = new HashMap<>();
     }
     
+    public void anadirEvento() throws IOException
+    {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        String ingresado;
+        Evento evento = new Evento();
+        
+        System.out.print("Ingresa el nombre del evento: ");
+        evento.setNombre(lector.readLine());
+        System.out.print("Ingresa la cantidad de asistentes al evento: ");
+        evento.setAsistentes(lector.readLine());
+        System.out.print("Ingresa el tipo de evento: ");
+        evento.setTipo(lector.readLine());
+        eventos.put(evento.getNombre(), evento);
+        
+        System.out.println("Evento agregado");
+    }
+    
+    public void mostrarEventos() throws IOException
+    {
+        eventos.forEach((k, v) ->
+        {
+            System.out.println();
+            System.out.print("Nombre: " + v.getNombre());
+            System.out.print(" Cantidad de asistente: " + v.getAsistentes());
+            System.out.println(" Tipo de evento: " + v.getTipo());
+        });
+    }
+    
+    // Setters y Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    public void setCapacidad(String capacidad) // Sobrecarga, por si el string contiene caracteres no validos
+    {
+        capacidad = capacidad.replaceAll("[^0-9]", " ");
+        capacidad = capacidad.replaceAll(" +", "");
+        System.out.println(capacidad);
+        this.capacidad = Integer.parseInt(capacidad);
+    }
+    
 }
